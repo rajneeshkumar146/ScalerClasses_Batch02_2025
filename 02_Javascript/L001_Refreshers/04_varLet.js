ae70e1280f1282c0856bd3589870af70738a2bc3
Rajneesh <rajneeshkumar146@gmail.com>
2025-08-04 23:36:08 +0530

    chunk 1

---
+++ 02_Javascript/L001_Refreshers/04_varLet.js
@@ -0,0 +1,44 @@
+// var a = 10;
+// console.log("line number 2", a);
+// function fn() {
+//     var a = 20;
+//     console.log("line number 4", a);
+//     a++;
+//     console.log("line number 7", a);
+//     if (a) {
+//         var a = 30;
+//         a++;
+//         console.log("line number 11", a);
+//     }
+//     console.log("line number 13", a);
+// }
+// fn();
+// console.log("line number 16", a);
+
+
+// let a = 10;
+// console.log("line number 2", a);
+// function fn() {
+//     let a = 20;
+//     console.log("line number 4", a);
+//     a++;
+//     console.log("line number 7", a);
+//     if (a) {
+//         let a = 30;
+//         a++;
+//         console.log("line number 11", a);
+//     }
+//     console.log("line number 13", a);
+// }
+// fn();
+// console.log("line number 16", a);
+
+
+
+// statement 2.
+var f = 5;
+function fn() {
+    f = 6;
+}
+fn()
+console.log(f)
