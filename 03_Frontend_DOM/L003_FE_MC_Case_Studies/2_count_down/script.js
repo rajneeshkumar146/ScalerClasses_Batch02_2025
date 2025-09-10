19c7c521b1ebf3dcef1b4ff04cca260a277bea93
Rajneesh <rajneeshkumar146@gmail.com>
2025-09-10 23:06:24 +0530

    JS2

--- 03_Frontend_DOM/L003_FE_MC_Case_Studies/2_count_down/script.js
+++ 03_Frontend_DOM/L003_FE_MC_Case_Studies/2_count_down/script.js
@@ -27,7 +27,41 @@ const hoursInput = document.getElementById("hr");
 const minutesInput = document.getElementById("min");
 const secondsInput = document.getElementById("sec");
 
+let SECONDS_IN_HOUR = 3600;
+let SECONDS_IN_MINUTE = 60;
+
+
 resetSetup();
+
+// Event Listners.
+startBtn.addEventListener("click", (event) => {
+    // 1. Take inputs and validate it.
+    let hours = getValidInput(hoursInput.value);
+    let minutes = getValidInput(minutesInput.value);
+    let seconds = getValidInput(secondsInput.value);
+
+    if (!validateInputTime(hours, minutes, seconds)) {
+        return;
+    }
+
+    let countDownTime = hours * SECONDS_IN_HOUR + minutes * SECONDS_IN_MINUTE + seconds;
+    runTimer(countDownTime);
+
+
+    pauseBtn.style.display = "block";
+    resetBtn.style.display = "block";
+
+    startBtn.style.display = "none";
+    continueBtn.style.display = "none";
+});
+
+pauseBtn.addEventListener("click", (e) => { });
+
+continueBtn.addEventListener("click", (e) => { });
+
+resetBtn.addEventListener("click", (event) => {
+    resetSetup();
+});
 
 // Helper methods.
 function resetSetup() {
@@ -42,11 +76,45 @@ function resetSetup() {
     resetBtn.style.display = "none";
 }
 
-function printOnConsole() {
+function runTimer(countDownTime) {
+    counterId = setInterval(() => {
+        updateUiEverySecond(countDownTime);
+        countDownTime--;
 
+        if(countDownTime < 0){
+            clearInterval(counterId);
+            return;
+        }
+
+    }, 1000);
+}
+
+function updateUiEverySecond(countDownTime) {
+    let hour = Math.floor(countDownTime / SECONDS_IN_HOUR);
+    let minute = Math.floor((countDownTime % SECONDS_IN_HOUR) / SECONDS_IN_MINUTE);
+    let second = Math.floor(countDownTime % SECONDS_IN_MINUTE);
+
+    console.log(hour + ":" + minute + ":" + second);
+}
+
+function getValidInput(value) {
+    return parseInt(!value ? 0 : value);
 }
 
 function validateInputTime(hours, minutes, seconds) {
+    if (hours < 0 || minutes < 0 || seconds < 0) {
+        alert("Negative value's are not allowed.");
+        return false;
+    } else if (hours > 24) {
+        alert("Hour is greater than 24 which is not a valid hour.");
+        return false;
+    } else if (minutes > 60) {
+        alert("Minute is greater than 60 which is not a valid minute.");
+        return false;
+    } else if (seconds > 60) {
+        alert("Second is greater than 60 which is not a valid second.");
+        return false;
+    }
 
-
+    return true;
 }
