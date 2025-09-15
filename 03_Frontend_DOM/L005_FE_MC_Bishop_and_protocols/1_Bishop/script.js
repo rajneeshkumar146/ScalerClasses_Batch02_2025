f3225f602b9e6cd1c24ab1512bd612f5cb5a831f
Rajneesh <rajneeshkumar146@gmail.com>
2025-09-15 21:58:20 +0530

    JS2

---
+++ 03_Frontend_DOM/L005_FE_MC_Bishop_and_protocols/1_Bishop/script.js
@@ -0,0 +1,46 @@
+
+let YELLOW_COLOR = "yellow";
+
+window.addEventListener("load", function () {
+    let table = this.document.querySelector("#table");
+    let N = 8, M = 8;
+
+    // chess grid creation.
+    // ri: row index, ci: column index.
+    for (let ri = 0; ri < N; ri++) {
+        let tr = this.document.createElement("tr");
+        let isWhite = ri % 2 === 0 ? true : false;
+        for (let ci = 0; ci < M; ci++) {
+            let cell = this.document.createElement("td");
+            cell.setAttribute("class", `box ${isWhite ? "white" : "black"}`);
+            cell.setAttribute("data-index", `${ri}-${ci}`);
+
+
+            tr.appendChild(cell);
+            isWhite = !isWhite;
+        }
+        table.appendChild(tr);
+    }
+
+    hoverEffect(table, N, M);
+
+});
+
+
+function hoverEffect(table, N, M) {
+    table.addEventListener("mouseover", (event) => {
+        let dataIndex = event.target.dataset.index;
+        if (dataIndex === undefined) {
+            return;
+        }
+
+        let [curr_row, curr_col] = dataIndex.split("-").map(idx => idx);
+        // console.log(curr_row + " " + curr_col);
+        possibleMoves(curr_row, curr_col, N, M);
+
+    });
+}
+
+function possibleMoves(curr_row, curr_col, N, M) {
+
+}
