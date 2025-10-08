2bfd770dec8fd678b0bacb1a2075e55f464c4f81
Rajneesh <rajneeshkumar146@gmail.com>
2025-10-08 22:26:09 +0530

    recat_04

--- 04_React/04_react_routing_feature/src/component/Home.jsx
+++ 04_React/04_react_routing_feature/src/component/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
   useEffect(() => {
     (async function () {
       const response = await fetch(`https://fakestoreapi.com/products`);
-      const productData = response.json();
+      const productData = await response.json();
 
       setProducts(productData)
     })();
@@ -28,19 +28,17 @@ function Home() {
           products === null ? <h3>...Loading</h3> :
             <>
               {
-                // console.log("Products: ", products)
-                console.log("Products: ", products)
-                // products.map((product) => {
-                //   return (
-                //     <div className='product'>
-                //       <img src={product.image} alt="" className='product_image'></img>
-                //       <div className='product_meta'>
-                //         <p className='product_title'>Title: {product.title}</p>
-                //         <p className='price'>Price: {product.price}</p>
-                //       </div>
-                //     </div>
-                //   );
-                // })
+                products.map((product) => {
+                  return (
+                    <div className='product'>
+                      <img src={product.image} alt="" className='product_image'></img>
+                      <div className='product_meta'>
+                        <p className='product_title'>Title: {product.title}</p>
+                        <p className='price'>Price: {product.price}</p>
+                      </div>
+                    </div>
+                  );
+                })
               }
             </>
 
