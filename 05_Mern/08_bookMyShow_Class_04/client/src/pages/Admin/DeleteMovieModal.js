81e6d3ccb5f2793fc1b1e899cd33fc65d963a5c1
Rajneesh <rajneeshkumar146@gmail.com>
2025-11-28 22:47:11 +0530

    bookmyshow_03

--- 05_Mern/08_bookMyShow_Class_04/client/src/pages/Admin/DeleteMovieModal.js
+++ 05_Mern/08_bookMyShow_Class_04/client/src/pages/Admin/DeleteMovieModal.js
@@ -14,15 +14,41 @@ const DeleteMovieModal = ({
     const dispatch = useDispatch();
 
     const handleOk = async () => {
+        try {
+            dispatch(ShowLoading());
+
+            const movieId = selectedMovie._id;
+            const response = await deleteMovie({ movieId });
+
+            if (response.success) {
+                message.success(response.message);
+                getData();
+            } else {
+                message.error(response.message);
+            }
+
+            setSelectedMovie(null);
+            setIsDeleteModalOpen(false);
+            dispatch(HideLoading());
+        } catch (err) {
+            message.error(err.message);
+            dispatch(HideLoading());
+            setIsDeleteModalOpen(false);
+        }
 
     }
 
     const handleCancel = () => {
-
+        setIsDeleteModalOpen(false);
+        setSelectedMovie(null);
     }
 
     return <Modal
-       
+        centered
+        title="Delete Movie"
+        open={isDeleteModalOpen}
+        onCancel={handleCancel}
+        onOk={handleOk}
     >
         Are you sure you want to delete this movie?
     </Modal>
